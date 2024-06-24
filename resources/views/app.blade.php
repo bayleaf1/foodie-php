a832c479b6b2684b82e195a9922c1835f8dc91b8
Bayleaf <152853920+bayleaf1@users.noreply.github.com>
2024-06-24 13:49:35 +0300

    fix

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -16,12 +16,12 @@
         href="https://fonts.googleapis.com/css2?family=Playwrite+NG+Modern:wght@100..400&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
         rel="stylesheet">
     @vite('resources/css/app.css')
+    <link rel="preload" as="style" href="https://foodie-php-dathlyo6wq-uc.a.run.app/build/assets/app-RqhUOUA-.css" />
+    <link rel="stylesheet" href="https://foodie-php-dathlyo6wq-uc.a.run.app/build/assets/app-RqhUOUA-.css" />
 </head>
 
 <body class="font-sans antialiased bg-white">
-    <p id="x">LALALAL</p>
     <div id="app"></div>
-    <p id="x">LALALAL</p>
     @vite('resources/js/app.js')
     <link rel="preload" as="style" href="https://foodie-php-dathlyo6wq-uc.a.run.app/build/assets/app-DuhB03Sn.css" />
     <link rel="modulepreload" href="https://foodie-php-dathlyo6wq-uc.a.run.app/build/assets/app-d3D1gYtE.js" />
