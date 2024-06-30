1dbe4dad85165bea6492ef64e6129588a6c09374
Bayleaf <152853920+bayleaf1@users.noreply.github.com>
2024-06-30 15:52:41 +0300

    refactor tests

--- tests/Feature/AcceptanceTests.php
+++ tests/Feature/AcceptanceTests.php
@@ -2,128 +2,13 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use Tests\SystemUser;
+use Tests\App;
+use Tests\Guest;
 
-class Entity
-{
-    public function __construct($ctx)
-    {
-        $this->ctx = $ctx;
-        $this->post_construct();
-    }
-
-    public function post_construct()
-    {
-    }
-}
-class SystemUser extends Entity
-{
-    public $headers = [];
-    public function getProfile()
-    {
-        $this->ctx->get(
-            '/api/system-users/profile',
-            $this->headers
-        )->assertJson([]);
-
-    }
-    public function post_construct()
-    {
-        $resp = $this->ctx->post(
-            '/api/auth/register/system-user',
-            [
-                "email" => "test@mail.com",
-                "name" => "Ion Mihai",
-                "password" => "qwer1234",
-                "c_password" => "qwer1234",
-            ]
-        )->json();
-
-        $this->headers = ["Authorization" => "Bearer " . $resp["accessToken"]];
-    }
-
-    public function create_products($products)
-    {
-        foreach ($products as $product) {
-            $this->ctx->post(
-                '/api/products',
-                ["images" => ["test_mock.jpg"], "description" => "test description", ...$product],
-                $this->headers
-            )->assertJson([]);
-
-        }
-    }
-    public function confirm_order($order_id)
-    {
-        $res = $this->ctx->patch(
-            '/api/orders/confirm/' . $order_id,
-            [],
-            $this->headers
-
-        );
-        return $res;
-    }
-    public function finalize_order($order_id)
-    {
-        $res = $this->ctx->patch(
-            '/api/orders/finalize/' . $order_id,
-            [],
-            $this->headers
-
-        );
-        return $res;
-
-    }
-
-    public function cancel_order($order_id)
-    {
-        $res = $this->ctx->patch(
-            '/api/orders/cancel/' . $order_id,
-            [],
-            $this->headers
-
-        );
-        return $res;
-    }
-}
-class Guest extends Entity
-{
-    public function create_order($order)
-    {
-        $res = $this->ctx->post('/api/orders', $order);
-        return $res;
-    }
-}
-class App extends Entity
-{
-    public $headers = [];
-
-    public function set_headers($v)
-    {
-        $this->headers = $v;
-    }
-    public function has_products($products)
-    {
-        $this->ctx->get('/api/products', $this->headers)->assertJson($products);
-    }
-    public function has_order($order)
-    {
-        $this->ctx->get('/api/orders', $this->headers)->assertJson([$order]);
-    }
-    public function has_orders($orders)
-    {
-        $this->ctx->get('/api/orders', $this->headers)->assertJson($orders);
-    }
-
-    public function respond_with_status($response, $status)
-    {
-        $response->assertStatus($status);
-    }
-}
-
-class ExampleTest extends TestCase
+class AcceptanceTests extends TestCase
 {
     use RefreshDatabase;
     public function setUp(): void
@@ -131,16 +16,7 @@ class ExampleTest extends TestCase
         parent::setUp();
         $this->refreshDatabase();
     }
-    // public function test_work_correct(): void
-    // {
-    //     $system_user = new SystemUser($this);
-    //     $app = new App($this);
-    //     $app->set_headers($system_user->headers);
 
-    //     $system_user->getProfile();
-    // }
-
-    //TODO reorganize tests
     public function test_product_quantity_is_restored_when_cancel_a_confirmed_order(): void
     {
         $system_user = new SystemUser($this);
